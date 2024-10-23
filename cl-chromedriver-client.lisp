ec3ccd1ee2a2a355aba6053d6cd395bfe64bc02b
Daniel Millward <dfmillward02@gmail.com>
2024-10-23 10:26:31 -0600

    Add wrapper around call to Chromedriver

--- cl-chromedriver-client.lisp
+++ cl-chromedriver-client.lisp
@@ -14,14 +14,27 @@
                                   ("password" . ,"testpassword")))
           :verbose t))
 
+
+(defmacro debug_call_driver (http_call)
+"Makes the passed dexador call. On 4xx or 5xx errors, displays helpful information."
+  `(handler-case
+       ,http_call
+     (dex:http-request-bad-request ()
+       (error "400 Error: A bad request was made to ChromeDriver!"))
+     (dex:http-request-failed (e)
+    ;; For other 4xx or 5xx
+    (format *error-output* "ChromeDriver returned ~D" (dex:response-status e)))))
+    
 ;;; https://lispcookbook.github.io/cl-cookbook/os.html#capturing-standard-and-error-output
-(defun check_for_chromedriver ()
+(defun start_chromedriver ()
   "Runs chromedriver --version to check if chromedriver is available. Throws an error if it fails."
   (handler-case
-      (uiop:run-program (list "chromedriver" "--version"))
+      (uiop:launch-program (list "chromedriver" "--port=4444"))
     (simple-error ()
-      (error "Can't run chromedriver. Is it on PATH?"))))
+      (error "Can't run chromedriver. Is it on PATH?"))
+    (uiop/run-program:subprocess-error ()
+      (error "Probably won't reach this because it just exits if port already bound."))))
 
 
 (defun main ()
-  (check_for_chromedriver))
+  (start_chromedriver))
