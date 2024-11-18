1dae97df0d82a8bca3393058a4ac13e3fd1ff0b5
Daniel Millward <dfmillward02@gmail.com>
2024-11-18 13:44:10 -0700

    Reformat and Add start-session function

--- cl-chromedriver-client.lisp
+++ cl-chromedriver-client.lisp
@@ -12,6 +12,30 @@
 (in-package #:cl-chromedriver-client)
 
 (defparameter *TEST-URI* "http://localhost:4444")
+(defparameter *CD-RUNNING-P* nil)
+
+(defun main ()
+  (start_chromedriver))
+
+;;; https://lispcookbook.github.io/cl-cookbook/os.html#capturing-standard-and-error-output
+(defun start_chromedriver ()
+  "Runs chromedriver --version to check if chromedriver is available. Throws an error if it fails."
+  (handler-case
+      (uiop:launch-program (list "chromedriver" "--port=4444"))
+    (simple-error ()
+      (error "Can't run chromedriver. Is it on PATH?"))
+    (uiop/run-program:subprocess-error ()
+      (error "Probably won't reach this because it just exits if port already bound.")))
+  (setf *CD-RUNNING-P* t))
+
+(defun start-session ()
+  "Starts a session with Chromedriver, starting Chromedriver if it's not started yet. Returns the session id."
+  (unless *CD-RUNNING-P* (start_chromedriver))
+  (extract_session_id_from_json 
+     (json_post "/session" (session_json))))
+
+(defun automate (test-domain &rest body)
+  body)
 
 (defun json_post (endpoint body)
   "Makes POST to Chromedriver on port 4444. Endpoint should be a string of format '/myendpoint' and body being a backtick'd object."
@@ -38,29 +62,18 @@
      (dex:http-request-failed (e)
     ;; For other 4xx or 5xx
     (format *error-output* "ChromeDriver returned ~D" (dex:response-status e)))))
-    
-;;; https://lispcookbook.github.io/cl-cookbook/os.html#capturing-standard-and-error-output
-(defun start_chromedriver ()
-  "Runs chromedriver --version to check if chromedriver is available. Throws an error if it fails."
-  (handler-case
-      (uiop:launch-program (list "chromedriver" "--port=4444"))
-    (simple-error ()
-      (error "Can't run chromedriver. Is it on PATH?"))
-    (uiop/run-program:subprocess-error ()
-      (error "Probably won't reach this because it just exits if port already bound."))))
 
 (defun session_json ()
-  `(("capabilities" . ,(make-hash-tablbe))))
+  `(("capabilities" . ,(make-hash-table))))
 
 (defun go_url_json (url)
   `(("url" . ,url)))
 
 (defun extract_session_id_from_json (response_json)
-  "Extracts session Id as string from Chromedriver response to POST /session in the form {'value': {'capabilities':{...},'sessionId': 'xxxxxx'}}"
-  (cdr (first (cdr (cdr (assoc :value (cl-json:decode-json-from-string response_json)))))))
+  "Extracts session Id as string from Chromedriver response to POST /session in the form {'value': {'capabilities':{...},'sessionId': 'xxxxxx'}}"  (cdr (first (cdr (cdr (assoc :value (cl-json:decode-json-from-string response_json)))))))
 
-(defun main ()
-  (start_chromedriver))
+
+
 
 ;;;; Make call with (extract_session_id_from_json (json_post "/session" (session_json)))
 
